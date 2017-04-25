606ede5ce52e15a184f1148cd2abc87e73dc748c
zengwenqi <zengwenqi@cloudchef.io>
2017-04-25 16:26:15 +0800

    rm the cloud code

--- cloudify-plugin/qcloud/get_vm.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-__author__ = 'Wayne'
-__date__ = '2017/3/9'
--- cloudify-plugin/vsphere/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-__author__ = 'Wayne'
-__date__ = '24/10/2016'
